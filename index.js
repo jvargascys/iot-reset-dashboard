803ab359a7a3dc8add37aaa169f40f857fff471b
System Administrator <jmunoz@cystecnologia.com>
2021-09-30 12:23:50 -0500

    Funcionalidad de encender/apagar led

--- index.js
+++ index.js
@@ -43,6 +43,31 @@ app.post('/api/reset-device/:deviceId', async(req, res) => {
         });
 });
 
+app.post('/api/turn-led-device/:deviceId/:status', async(req, res) => {
+    const deviceId = req.params.deviceId;
+    const status = req.params.status;
+
+    console.log(deviceId);
+
+    await axios.post("http://20.65.118.16:8081/api/v4/mqtt/publish", {
+            "topic": "turn.led.device/" + deviceId,
+            "payload": status,
+            "qos": 1,
+            "retain": false
+        }, {
+            auth: {
+                username: "admin",
+                password: "public"
+            }
+        })
+        .then(data => {
+            res.send(data.data)
+        })
+        .catch(err => {
+            res.send(err).status(500);
+        });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
